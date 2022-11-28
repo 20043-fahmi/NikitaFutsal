ff72ae0fe33d81e1cea88546dfbf26f9eed566f9
20043 <fahmi.muhazir.12@gmail.com>
2022-11-28 22:43:12 +0700

    push project mppl

---
+++ resources/views/theme/theme.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+    <link rel="stylesheet" href="{{asset("css/appku.css")}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @yield('css')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>Neofutsal - @yield('title')</title>
+</head>
+
+<body class="body">
+    {{-- Navbar --}}
+    @include('theme.navbar')
+    {{-- End Navbar --}}
+    <div class="mx-auto bg-gray-100 rounded-md  overflow-hidden">
+        {{-- min-h-screen --}}
+        {{-- Content --}}
+        <div id="content" class="relative">
+            {{-- Content --}}
+
+                @yield('content')
+            
+            {{-- End of Content --}}
+        </div>
+    </div>
+{{-- Footer --}}
+<div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
+  
+      <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      </a>
+  
+      <ul class="nav col-md-4 justify-content-end">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+      </ul>
+    </footer>
+  </div>
+{{-- End of Footer --}}
+
+{{--  --}}
+
+{{--  --}}
+    {{-- <script src="{{asset("js/appku.js")}}"></script> --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    {{-- Font Awesome --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"
+        integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    {{-- Sweetalert2 --}}
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <script>
+        let toastr = (type, msg, btn=null)=>{
+            Swal.fire({
+                icon : type,
+                html : msg,
+                showConfirmButton : (btn == null ? false : true),
+                confirmButtonText : btn,
+            })
+        }
+    </script>
+    <script>
+        $(document).ready(function(){
+            let isNotifOpen = false;
+            $('#btn-notification').click(function(){
+                if(!isNotifOpen){
+                    $(this).addClass('text-primary').removeClass('text-gray-400');
+                    $('#notification-card').removeClass('hidden');
+                }else{
+                    $(this).removeClass('text-primary').addClass('text-gray-400');
+                    $('#notification-card').addClass('hidden');
+                }
+                isNotifOpen = !isNotifOpen;
+            })
+        })
+    </script>
+    @yield('js')
+    @if ($msg = session()->get('success'))
+    <script>
+        toastr('success', `{{ $msg }}`);
+    </script>
+    @elseif ($msg = session()->get('errors'))
+    <script>
+        toastr('error', `{!! $msg !!}`,`Saya Paham`);
+    </script>
+    @endif
+</body>
+
+</html>
---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,96 @@
+@extends('theme.theme')
+@section('title','Booking Online Futsal')
+@section('content')
+
+{{-- Mainpage Content --}}
+{{-- Slider --}}
+{{-- <div id="slider" class="my-3">
+    <div class="swiper-container h-36 md:h-64">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+                <img src="{{ asset('images/banner/banner-1.png') }}" class="object-cover h-36 md:h-64 w-full rounded"
+                    alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="{{ asset('images/banner/banner-2.png') }}" class="object-cover h-36 md:h-64 w-full rounded"
+                    alt="">
+            </div>
+        </div>
+    </div>
+</div> --}}
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="dark">
+          <img src="images/Carousel1.png" class="d-block img-carousel w-100" alt="...">
+        </div>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="dark">
+          <img src="images/Carousel2.png" class="d-block img-carousel w-100" alt="...">
+        </div>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="dark">
+          <img src="images/Carousel.png" class="d-block img-carousel w-100" alt="...">
+        </div>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <!-- {{-- End Carousel --}} -->
+  
+{{-- End of Slider --}}
+<div class="produck-dashboard ">
+  <h1 class="text-md text-dark font-semibold border-b-2 pb-3 ">Booking Now!</h1>
+  <div class="my-3 grid grid-flow-row grid-cols-1 md:grid-cols-3 gap-4 auto-rows-max ">
+      @foreach ($futsal_fields as $field)
+      <x-product-card :field="$field" />
+      @endforeach
+  </div>
+</div>
+
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+@endsection
+@section('js')
+{{-- <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script>
+    const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay: true,
+        delay : 2000,
+        });
+</script> --}}
+@endsection
